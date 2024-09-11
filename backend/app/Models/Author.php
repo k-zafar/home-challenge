6decd265cd8a763ba22e7954fa06eccd1305c104
Kamran Zafar <kamran.zafar@cognitiveaxis.com>
2024-09-11 16:17:13 +0500

    migrations created along with the models

---
+++ backend/app/Models/Author.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    public function article(): HasOne
+    {
+        return $this->hasOne(Article::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'author_user');
+    }
+}
